68fb6ce56e9b42fbe4f0fa746135646ea1a18882
Benjamin Santiago Calderon <40007797+bscalder@users.noreply.github.com>
2024-07-09 11:45:52 -0600

    Seguimos trabajando en el Modal ...

--- src/components/ModalEditar/index.jsx
+++ src/components/ModalEditar/index.jsx
@@ -5,16 +5,17 @@ const ModalEditar = () => {
         <section className={style.modaleditar}>
             <div>EDITAR CARD:</div>
             <form>
-                <div><div><label htmlFor="titulo">Titulo</label></div><input type="text" id="titulo" required /></div>
+                <div><div><label htmlFor="titulo">Titulo</label></div><input type="text" id="titulo" placeholder="algo" required /></div>
                 <div><div><label htmlFor="categoria">Categoria</label></div><select name="categoria" id="categoria" required>
                     <option value=""></option>
                     <option value="FRONTEND">FRONTEND</option>
                     <option value="BACKEND">BACKEND</option>
                     <option value="INOVACION Y GESTION">INOVACION Y GESTION</option>
                 </select></div>
-                <div><div><label htmlFor="imagen">Imagen</label></div><input type="text" id="imagen" required /></div>
-                <div><div><label htmlFor="video">Video</label></div><input type="text" id="video" required /></div>
-                <div><div><label htmlFor="descripcion">Descripcion</label></div><textarea name="descripcion" id="descripcion"></textarea></div>
+                <div><div><label htmlFor="imagen">Imagen</label></div><input type="text" id="imagen" placeholder="algo" required /></div>
+                <div><div><label htmlFor="video">Video</label></div><input type="text" id="video" placeholder="algo" required /></div>
+                <div><div><label htmlFor="descripcion">Descripcion</label></div><textarea name="descripcion" id="descripcion">algo</textarea></div>
+                <div className={style.botones}><input type="submit" value="GUARDAR" /><input type="reset" value="LIMPIAR" /></div>
             </form>
         </section>
     )
